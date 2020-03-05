8d4e157b9b7b880852cf06c939f69be8aac7ab07
Madoxen <pchlad5@gmail.com>
2020-03-05 01:33:35 +0100

    Added basic algs and started main project

---
+++ iris_classifier/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using ML.Lib;
+
+
+
+
+namespace iris_classifier
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+
+
+
+
+        
+    }
+}
---
+++ normalization/Normalizator.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace ML.Lib
+{
+    class Normalizator
+    {
+        static void Main(string[] args)
+        {
+            double[] set = {1.0, 2.0, 3.0, 4.0, 5.0, 5.0, 4.0};
+            double[] normalized = Normalize(set, 1.0, 0.0);
+            for (int i = 0; i < normalized.Length; i++)
+            {
+                Console.Write(normalized[i] + " ");
+            }
+            
+        }
+
+        static double Max(double[] input)
+        {
+            double result = double.MinValue;
+            for (int i = 0; i < input.Length; i++)
+            {
+                if(result < input[i])
+                    result = input[i];
+            }
+            return result;
+        }
+
+        static double Min(double[] input)
+        {
+            double result = double.MaxValue;
+            for (int i = 0; i < input.Length; i++)
+            {
+                if(result > input[i])
+                    result = input[i];
+            }
+            return result;
+        }
+
+        static double[] Normalize(double[] input, double nmax, double nmin)
+        {
+            double[] result = new double[input.Length];
+            double min = Min(input);
+            double max = Max(input);
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                result[i] = ((input[i] - min) / (max - min)) * (nmax - nmin) + nmin;
+            }
+            return result;
+        }
+
+
+
+    }
+}
